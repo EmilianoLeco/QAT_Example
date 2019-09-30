188233b55f9bd426f3a07dd38e86046eba055c6b
Emiliano lecoña millahuel <emilianoleco@gmail.com>
2019-09-30 11:15:19 -0300

    "Relative path chromedriver.exe"

--- features/browser.py
+++ features/browser.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
+from os.path import dirname
+import os
+
+dirpath = os.path.abspath(os.path.dirname(__file__))
+dirname(dirpath)
+chromedriver_path = (dirname(dirpath)) + "\\chromedriver.exe"
+
+print(chromedriver_path)
 
 class Browser(object):
-    driver = webdriver.Chrome('chromedriver.exe')
+    driver = webdriver.Chrome(chromedriver_path)
     driver.implicitly_wait(30)
     driver.set_page_load_timeout(30)
     driver.maximize_window()
