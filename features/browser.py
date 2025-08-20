64a38f42d7d12d7a0d1e7d22cdbbb0f3143590f4
EmilianoLeco <emilianoleco@gmail.com>
2025-08-20 14:05:25 -0300

    Refactor browser to initialize driver instance

--- features/browser.py
+++ features/browser.py
@@ -3,16 +3,15 @@ from os.path import dirname
 import os
 
 dirpath = os.path.abspath(os.path.dirname(__file__))
-dirname(dirpath)
-chromedriver_path = (dirname(dirpath)) + "\\chromedriver.exe"
+chromedriver_path = os.path.join(dirname(dirpath), "chromedriver.exe")
 
-print(chromedriver_path)
 
 class Browser(object):
-    driver = webdriver.Chrome(chromedriver_path)
-    driver.implicitly_wait(30)
-    driver.set_page_load_timeout(30)
-    driver.maximize_window()
+    def __init__(self):
+        self.driver = webdriver.Chrome(chromedriver_path)
+        self.driver.implicitly_wait(30)
+        self.driver.set_page_load_timeout(30)
+        self.driver.maximize_window()
 
-    def close(context):
-        context.driver.quit()
+    def close(self):
+        self.driver.quit()
